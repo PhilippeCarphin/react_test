6662defabc60d340b1b66d8213f4212945293a1a
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-08 03:28:47 -0500

    Changed alignment in Figure

--- src/Figure.js
+++ src/Figure.js
@@ -4,15 +4,24 @@ import { Text , View , Image} from 'react-native'
 class Figure extends Component {
   render() {
     return (
-            <View>
-            <Image source={require('./TPOP.jpg')} style={{width: 100, height:100}} />
-            <Text> {this.props.caption} </Text>
+            <View style={styles.figure_style}>
+              <Image source={require('./TPOP.jpg')} style={{width: 100, height:100}} />
+              <Text> {this.props.caption} </Text>
             </View>
             // The following line doesn't work but the caption thing works.  I was expecting to
             // be able to use this.props.filename like I did with the caption but it doesn't work
             // that way it seems.  I should try to understand javascript.
             // <Image source={require(this.props.filename)} style={{width: 100, height:100}} />
             );
+  }
+}
+
+const styles = {
+  figure_style : {
+    justifyContent : 'center',
+    alignItems : 'center',
+    borderWidth : 2,
+    borderRadius : 5
   }
 }
 
