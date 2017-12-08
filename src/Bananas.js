e6f72d518e95e1d394cb7c93ffaf3b135b64170a
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-08 02:31:05 -0500

    Added styling to bananas

--- src/Bananas.js
+++ src/Bananas.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react'
-import { Image } from 'react-native'
+import { Image, View } from 'react-native'
 
 class Bananas extends Component {
   render() {
@@ -7,8 +7,32 @@ class Bananas extends Component {
     uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-            <Image source={pic} style={{width: 193, height: 110}}/>
-            );
+      <View style={styles.view_style}>
+            <Image source={pic} style={styles.image_style}/>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  view_style: {
+    backgroundColor: '#50A0D0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 15, height: 50
+    },
+    width : 220,
+    height : 135
+  },
+  image_style: {
+    width: 193,
+    height: 110,
+    shadowColor: '#FF0000',
+    shadowOffset: {
+      width: 15, height: 50
+    },
   }
 }
 
