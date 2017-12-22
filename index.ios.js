abfa2b8696500b76d24db4cbd382536522b768d7
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-22 17:13:56 -0500

    Scrollview try working

--- index.ios.js
+++ index.ios.js
@@ -3,19 +3,41 @@ import {
   AppRegistry,
   Text,
   View,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
 
 import RandomThing from './src/RandomThing.js'
 
+import Figure from './src/Figure'
+import Bananas from './src/Bananas'
 
-react_test = () => (
-  <View style={styles.view_style}>
-      <RandomThing/>
-      <Text>'=========='</Text>
-      <RandomThing/>
-  </View>
-)
+
+react_test = () => {
+  return (
+    <View style={styles.view_style}>
+      <ScrollView style={{flex : 1}}>
+        <Figure caption='Figure 1'/>
+        <Text>"Texte entre les figures"</Text>
+        <Figure caption='Figure 2'/>
+        <Text>"Texte entre les figures"</Text>
+        <Figure caption='Figure 3'/>
+        <Text>"Texte entre les figures"</Text>
+        <Figure caption='Figure 4'/>
+        <Text>"Texte entre les figures"</Text>
+        <Figure caption='Figure 5'/>
+        <Text>"Texte entre les figures"</Text>
+        <Figure caption='Figure 6'/>
+        <Text>"Texte entre les figures"</Text>
+        <Figure caption='Figure 7'/>
+        <Text>"Texte entre les figures"</Text>
+        <Figure caption='Figure 8'/>
+        <Text>"Texte entre les figures"</Text>
+        <Figure caption='Figure 9'/>
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = {
   view_style: {
