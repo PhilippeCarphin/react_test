243519c2df516a0d32fbf1e47ac0da15792a63a6
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-01-31 20:14:17 -0500

    Using RandomThing component

--- index.ios.js
+++ index.ios.js
@@ -18,22 +18,10 @@ react_test = () => {
     <View style={styles.view_style}>
       <ScrollView style={{flex : 1}}>
         <Figure caption='Figure 1'/>
-        <Text>"Texte entre les figures"</Text>
-        <Figure caption='Figure 2'/>
-        <Text>"Texte entre les figures"</Text>
-        <Figure caption='Figure 3'/>
-        <Text>"Texte entre les figures"</Text>
-        <Figure caption='Figure 4'/>
-        <Text>"Texte entre les figures"</Text>
-        <Figure caption='Figure 5'/>
-        <Text>"Texte entre les figures"</Text>
-        <Figure caption='Figure 6'/>
-        <Text>"Texte entre les figures"</Text>
-        <Figure caption='Figure 7'/>
-        <Text>"Texte entre les figures"</Text>
-        <Figure caption='Figure 8'/>
-        <Text>"Texte entre les figures"</Text>
-        <Figure caption='Figure 9'/>
+        <RandomThing/>
+
+        <RandomThing/>
+        <RandomThing/>
       </ScrollView>
     </View>
   );
